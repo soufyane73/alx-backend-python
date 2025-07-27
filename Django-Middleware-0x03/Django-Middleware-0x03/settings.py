f00a5e56ee1fbf9a2f62ff47173e3055c882988b
Soufyane Bougida <82409191+soufyane73@users.noreply.github.com>
2025-07-27 13:25:54 +0100

    Update settings.py

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'chats.middleware.RolepermissionMiddleware',
     "chats.middleware.RestrictAccessByTimeMiddleware",
     "chats.middleware.OffensiveLanguageMiddleware",
+    "chats.middleware.RequestLoggingMiddleware",
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
