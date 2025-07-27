4f4ee4438da350e39ab475270d74307fa381c550
Soufyane Bougida <82409191+soufyane73@users.noreply.github.com>
2025-07-27 12:58:14 +0100

    Update settings.py

--- 0x03-MessagingApp-Django/Django-Middleware-0x03/settings.py
+++ 0x03-MessagingApp-Django/Django-Middleware-0x03/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'chats.middleware.RequestLoggingMiddleware',
-    'chats.middleware.RestrictAccessByTimeMiddleware',
+    "chats.middleware.RestrictAccessByTimeMiddleware",
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
